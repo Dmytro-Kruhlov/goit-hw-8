203578548b5f7f746b921aa5c686f9599e3deddd
Dmytro-Kruhlov <hazzy1451@gmail.com>
2023-10-07 20:42:15 +0300

    add regex for function and add cache

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
-
 from models import Quotes, Authors
+import redis
+from redis_lru import RedisLRU
+
+
+client = redis.StrictRedis(host="localhost", port=6379, password=None)
+cache = RedisLRU(client)
+
+
+@cache
+def quote_by_tag(com):
+    tag = command.split("tag:")[1].strip()
+    quotes = Quotes.objects(tags__iregex=f".*{tag}.*")
+    for quote in quotes:
+        print(quote.quote)
+
+@cache
+def quote_by_author(com):
+    author_name = command.split("name:")[1].strip()
+    author = Authors.objects(fullname__iregex=f"^{author_name}.*").first()
+    if author:
+        quotes = Quotes.objects(author=author)
+        for quote in quotes:
+            print(quote.quote)
+    else:
+        print("Автор не знайдений")
 
 
 if __name__ == "__main__":
-
     while True:
         command = input("Введіть команду: ")
 
         if command.startswith("name:"):
-            author_name = command.split("name:")[1].strip()
-            author = Authors.objects(fullname=author_name).first()
-            if author:
-                quotes = Quotes.objects(author=author)
-                for quote in quotes:
-                    print(quote.quote)
-            else:
-                print("Автор не знайдений")
+            quote_by_author(command)
 
         elif command.startswith("tag:"):
-            tag = command.split("tag:")[1].strip()
-            quotes = Quotes.objects(tags=tag)
-            for quote in quotes:
-                print(quote.quote)
+            quote_by_tag(command)
 
         elif command.startswith("tags:"):
             tags = command.split("tags:")[1].strip().split(",")
